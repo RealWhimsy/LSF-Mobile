0a2c8ddecab6a08aac384aedc792c897c6f1f9db
kappa4head <lukas.jack@web.de>
2019-08-27 01:13:31 +0200

    Removed old TODOs

--- js/timetableOverlayController.js
+++ js/timetableOverlayController.js
@@ -1,7 +1,4 @@
 
-// TODO add lectures to a custom JSON-Object on click. Delete if clicked again
-// TODO Modify origin of timetable source in /timetable/timetable.js , Line 635 and 637. Use own custom Object instead
-
 function showTimetable() {
     document.getElementById("overlay").style.display = "block";
 }
