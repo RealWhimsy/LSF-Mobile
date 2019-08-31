756477fe8a6c1e155d2ebf6c9c4abd669e744b6d
kappa4head <lukas.jack@web.de>
2019-08-31 12:20:14 +0200

    Timetable and course list view can now be toggled by buttons placed ontop of the list

--- js/OverlayController.js
+++ js/OverlayController.js
@@ -30,3 +30,29 @@ function hideOverlay(overlay) {
         domParent.removeChild(overlay);
     }
 }
+
+function toggleMainView(button) {
+    // If the already selected button is clicked again, the function should not toggle anything ==> return
+    if(button.classList.contains('selected')){
+        return;
+    }
+    var courseButton = document.getElementById('courseButton');
+    var timetableButton = document.getElementById('timetableButton');
+
+    var courseList = document.getElementById('accordion-list');
+    var timetable = document.getElementById('timetable-1');
+
+    if(courseButton.classList.contains('selected')) {
+        courseButton.classList.remove('selected');
+        timetableButton.classList.add('selected');
+
+        courseList.style.display = 'none';
+        timetable.style.display = 'block';
+    } else {
+        courseButton.classList.add('selected');
+        timetableButton.classList.remove('selected');
+
+        courseList.style.display = 'block';
+        timetable.style.display = 'none';
+    }
+}
